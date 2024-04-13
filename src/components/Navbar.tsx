b71885955274370be8f024c6252acfd99350b53e
prince saspara <princesaspara46@gmail.com>
2024-04-13 09:54:36 +0530

    next js explorer

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import React, { useState } from "react";
+
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+
+
+const Navbar = ({ className }: { className?: string }) => {
+  const [active, setActive] = useState<string | null>(null);
+  
+  return (
+    <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
+      <Menu setActive={setActive}>
+        <Link href={'#'}>
+          <MenuItem setActive={setActive} active={active} item='Home'>
+
+          </MenuItem>
+          <MenuItem setActive={setActive} active={active} item='Services'>
+            <div className="flex flex-col space-y-4 text-sm">
+
+              <HoveredLink href='/courses'>
+                All courses
+
+              </HoveredLink>
+              <HoveredLink href='/courses'>
+                All courses
+
+              </HoveredLink>
+              <HoveredLink href='/courses'>
+                All courses
+
+              </HoveredLink>
+              <HoveredLink href='/courses'>
+                All courses
+
+              </HoveredLink>
+              <HoveredLink href='/courses'>
+                All courses
+
+              </HoveredLink>
+            </div>
+
+          </MenuItem>
+        </Link>
+      </Menu>
+    </div>
+  )
+}
+
+export default Navbar
