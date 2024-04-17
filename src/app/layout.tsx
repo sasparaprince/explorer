4dc419994554bd1928cd8caad786eaf59ce0d796
prince saspara <princesaspara46@gmail.com>
2024-04-17 11:58:53 +0530

    hero section complete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,13 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>
-        <div className="relative w-full flex items-center justify-center">
+    <body className={inter.className}>
+      <div className="relative w-full flex items-center justify-center ">
         <Navbar />
-
-        </div>
-        {children}
-        </body>
-    </html>
+      </div>
+      {children}
+    </body>
+  </html>
   );
 }
+ 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import HeroSection from "@/components/HeroSection";
+import FeaturedCourses from "@/components/FeaturedCourses";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 export default function Home() {
   return (
-<main className="min-h-screen bg-black/[0.96] antialiased bg-gried-white/[0.02]">
-  <h1 className="text-2xl text-center">Prince Saspara</h1>
+<main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+
+  <HeroSection />
+  <FeaturedCourses />
+  <WhyChooseUs />
+
+
 </main>
    
   );
